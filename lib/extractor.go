d0369434d28c5b1b4bb08a7a3a25cf194d886c70
Alex Ferrari <alex@thealexferrari.com>
2023-07-16 10:50:22 +0200

    Refactor extractor for legibility

--- lib/extractor.go
+++ lib/extractor.go
@@ -3,7 +3,9 @@ package lib
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
@@ -29,20 +31,20 @@ func (r *RawPost) ToPost() (Post, error) {
 }
 
 type Post struct {
-	Id               int      `json:"id"`
-	PublicationId    int      `json:"publication_id"`
-	Type             string   `json:"type"`
-	Slug             string   `json:"slug"`
-	PostDate         string   `json:"post_date"`
-	CanonicalUrl     string   `json:"canonical_url"`
-	PreviousPostSlug string   `json:"previous_post_slug"`
-	NextPostSlug     string   `json:"next_post_slug"`
-	CoverImage       string   `json:"cover_image"`
-	Description      string   `json:"description"`
-	WordCount        int      `json:"wordcount"`
-	PostTags         []string `json:"postTags"`
-	Title            string   `json:"title"`
-	BodyHTML         string   `json:"body_html"`
+	Id               int    `json:"id"`
+	PublicationId    int    `json:"publication_id"`
+	Type             string `json:"type"`
+	Slug             string `json:"slug"`
+	PostDate         string `json:"post_date"`
+	CanonicalUrl     string `json:"canonical_url"`
+	PreviousPostSlug string `json:"previous_post_slug"`
+	NextPostSlug     string `json:"next_post_slug"`
+	CoverImage       string `json:"cover_image"`
+	Description      string `json:"description"`
+	WordCount        int    `json:"wordcount"`
+	//PostTags         []string `json:"postTags"`
+	Title    string `json:"title"`
+	BodyHTML string `json:"body_html"`
 }
 
 func (p *Post) ToMD(withTitle bool) (string, error) {
@@ -97,7 +99,7 @@ func (p *Post) WriteToFile(path string, format string) error {
 	case "md":
 		content, err = p.ToMD(true)
 		if err != nil {
-			panic(err)
+			return err
 		}
 	case "txt":
 		content = p.ToText(true)
@@ -106,10 +108,13 @@ func (p *Post) WriteToFile(path string, format string) error {
 	}
 	_, err = f.WriteString(content)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
-	f.Sync()
+	err = f.Sync()
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
@@ -129,20 +134,7 @@ func NewExtractor(f *Fetcher) *Extractor {
 	return &Extractor{fetcher: f}
 }
 
-func (e *Extractor) ExtractPost(ctx context.Context, pageUrl string) (Post, error) {
-	// fetch page HTML content
-	body, err := e.fetcher.FetchURL(ctx, pageUrl)
-	if err != nil {
-		return Post{}, err
-	}
-	defer body.Close()
-
-	doc, err := goquery.NewDocumentFromReader(body)
-	if err != nil {
-		return Post{}, err
-
-	}
-
+func findScriptContent(doc *goquery.Document) string {
 	scriptContent := ""
 	doc.Find("script").EachWithBreak(func(i int, s *goquery.Selection) bool {
 		if strings.Contains(s.Text(), "window._preloads") && strings.Contains(s.Text(), "JSON.parse(") {
@@ -151,30 +143,74 @@ func (e *Extractor) ExtractPost(ctx context.Context, pageUrl string) (Post, erro
 		}
 		return true
 	})
+	return scriptContent
+}
 
-	start := strings.Index(scriptContent, "JSON.parse(\"") + len("JSON.parse(\"")
+func extractJSONString(scriptContent string) (string, error) {
+	start := strings.Index(scriptContent, "JSON.parse(\"")
 	end := strings.LastIndex(scriptContent, "\")")
-	jsonString := scriptContent[start:end]
+
+	if start == -1 || end == -1 || start >= end {
+		return "", errors.New("failed to extract JSON string")
+	}
+
+	return scriptContent[start+len("JSON.parse(\"") : end], nil
+}
+
+func (e *Extractor) ExtractPost(ctx context.Context, pageUrl string) (Post, error) {
+	// fetch page HTML content
+	body, err := e.fetcher.FetchURL(ctx, pageUrl)
+	if err != nil {
+		return Post{}, fmt.Errorf("failed to fetch page: %s", err)
+	}
+	defer body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(body)
+	if err != nil {
+		return Post{}, fmt.Errorf("failed to fetch page: %s", err)
+
+	}
+
+	scriptContent := findScriptContent(doc)
+
+	if scriptContent == "" {
+		return Post{}, fmt.Errorf("failed to fetch page: script content not found")
+	}
+
+	jsonString, err := extractJSONString(scriptContent)
+	if err != nil {
+		return Post{}, fmt.Errorf("failed to fetch page: %s", err)
+	}
 
 	// jsonString is a stringified JSON string. Convert it to a normal JSON string
 	var rawJSON RawPost
 	err = json.Unmarshal([]byte("\""+jsonString+"\""), &rawJSON.str) //json.NewEncoder(&rawJSON).Encode([]byte("\"" + jsonString + "\""))
 	if err != nil {
-		return Post{}, err
+		return Post{}, fmt.Errorf("failed to fetch page: %s", err)
 	}
 
 	// Now convert the normal JSON string to a Go object
 	p, err := rawJSON.ToPost()
 	if err != nil {
-		return Post{}, err
+		return Post{}, fmt.Errorf("failed to fetch page: %s", err)
 	}
 
 	return p, nil
 }
 
 func (e *Extractor) GetAllPostsURLs(ctx context.Context, pubUrl string) ([]string, error) {
+	u, err := url.Parse(pubUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	u.Path, err = url.JoinPath(u.Path, "sitemap.xml")
+	if err != nil {
+		return nil, err
+	}
+
 	// fetch the sitemap of the publication
-	body, err := e.fetcher.FetchURL(ctx, pubUrl+"/sitemap.xml")
+	body, err := e.fetcher.FetchURL(ctx, u.String())
 	if err != nil {
 		return nil, err
 	}
@@ -187,11 +223,19 @@ func (e *Extractor) GetAllPostsURLs(ctx context.Context, pubUrl string) ([]strin
 	}
 
 	urls := []string{}
-	doc.Find("loc").Each(func(i int, s *goquery.Selection) {
+	doc.Find("loc").EachWithBreak(func(i int, s *goquery.Selection) bool {
+		// Check if the context has been cancelled
+		select {
+		case <-ctx.Done():
+			return false
+		default:
+		}
 		url := s.Text()
 		if strings.Contains(url, "/p/") {
 			urls = append(urls, url)
 		}
+
+		return true
 	})
 
 	return urls, nil
