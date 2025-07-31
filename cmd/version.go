79ec59f4cd89b740ce5463915d697af721ad7234
Alex Ferrari <alex@thealexferrari.com>
2025-07-31 21:05:35 +0200

    chore: bump version to v0.6.5

--- cmd/version.go
+++ cmd/version.go
@@ -12,7 +12,7 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of sbstck-dl",
 	Long:  `Display the current version of the app.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("sbstck-dl v0.6.4")
+		fmt.Println("sbstck-dl v0.6.5")
 	},
 }
 
